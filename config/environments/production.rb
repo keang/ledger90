3a26106de702c2bbce5be0af606a2a6cb7e1b957
keang <skeang@gmail.com>
2016-12-28 01:40:21 +0800

    Add mailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'ledger90.herokuapp.com', port: 443 }
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :domain => ENV["ledger90.herokuapp.com"],
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
